d63bf0e036f2277f4f2f8fa760cf6a1cfa9fe886
ycl <y1207544655@163.com>
2021-01-27 23:45:25 +0800

    first

---
+++ src/main/java/com/example/springbootsimpledemo/SpringbootSimpleDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootsimpledemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSimpleDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootSimpleDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootsimpledemo/SpringbootSimpleDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootsimpledemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSimpleDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
